e58f22018af88d905e7193eb75c597ff40415557
Christa Ringers <christanellekeringers@gmail.com>
2021-11-14 21:28:43 +0100

    Add existing file

--- Master_analysis.m
+++ Master_analysis.m
@@ -4,13 +4,13 @@
 %% Step 0: Load the aligned recording 
 
 % Define the path to the raw recording
-SourceP = 'C:\Users\chririn\OneDrive - NTNU\Github\03-02-2017_0_0_aligned.mat'; 
+SourceP = 'C:\Users\Bonnie\OneDrive - NTNU\Github\03-02-2017_0_0_aligned.mat'; 
 CBF.name = '03-02-2017_0_0_aligned'; % Give a name
 load(SourceP); 
 data = double(aligned); 
 
 % Define the target path 
-CBF.targetP = 'C:\Users\chririn\OneDrive - NTNU\Github\03-02-2017_0_0_aligned\';
+CBF.targetP = 'C:\Users\Bonnie\OneDrive - NTNU\Github\03-02-2017_0_0_aligned\';
 mkdir(CBF.targetP)
 cd(CBF.targetP); 
 
@@ -18,6 +18,7 @@ cd(CBF.targetP);
 CBF.Fs = 107; % Frequency of acquisition
 
 save([CBF.targetP, CBF.name, '_CBF_parameters'], 'CBF');
+
 %% Step 1: Defining variables 
  
 % Define variables and save them in one common structure
@@ -26,6 +27,8 @@ CBF.y = size(data,2);
 CBF.w_min = 15; % Lower frequency cutoff 
 CBF.caxis = [16 40]; % Upper and lower bound for any frequency plot. 
 CBF.spatres = 0.15;  % [um/pixel] spatial resolution
+CBF.signal_size = 500; % minimum number of pixels to be considered signal
+CBF.SD = 3; % maximum standard deviation for a block of 9 pixels to be considered signal
 
 % Save the results
 save([CBF.targetP, CBF.name, '_CBF_parameters'], 'CBF');
@@ -33,20 +36,20 @@ save([CBF.targetP, CBF.name, '_CBF_parameters'], 'CBF');
 %% Step 2: Perform the fast Fourier transform 
 
 % Fourier Transform (~20s to run the fft & ~200s with plotting)
-[PowerSpec,~,~,~,~,CBF.picSD,~,CBF.mask,CBF.nframe, PSD, PeakPos] = bmf_sw_cr_fft_analysis3(data,CBF);
+[PowerSpec,~,~,~,~,CBF.picSD,~,CBF.mask,CBF.nframe, PSD, PeakPos] = bmf_sw_cr_fft_analysis4(data,CBF);
 
 % It is possible to run the frequency analysis with a predefined mask. 
-% [PowerSpec,~,~,~,~,CBF.picSD,~,CBF.mask,CBF.nframe, PSD, PeakPos] = bmf_sw_cr_fft_analysis3(data,CBF, mask);
+% [PowerSpec,~,~,~,~,CBF.picSD,~,CBF.mask,CBF.nframe, PSD, PeakPos] = bmf_sw_cr_fft_analysis4(data,CBF, mask);
 
 % Save the results
-save([CBF.targetP, CBF.name, '_CBF_parameters'], 'CBF');
+% save([CBF.targetP, CBF.name, '_CBF_parameters'], 'CBF');
 
 %% Step 3: Coherence with example pixels 
  
 % Decide on reference pixels: uncomment to choose
-figure, imagesc(CBF.picSD); 
-ref = ginput; 
-ref = round(ref);
+% figure, imagesc(CBF.picSD); 
+% ref = ginput; 
+% ref = round(ref);
 CBF.ref = ref; % Save the refence pixels
 
 % Set some variables specific to the coherence analysis
@@ -58,10 +61,10 @@ CBF.nfft = 100;
 [Pxx, val] = cr_coherence_ref(data, CBF);
 
 % Save results
-save([CBF.targetP, CBF.name, '_CBF_parameters'], 'CBF');
+% save([CBF.targetP, CBF.name, '_CBF_parameters'], 'CBF');
 
 %% Step 3.5: Coherence versus spectral density scatterplot 
-% 
+
 % % Plot coherence versus power spectral contribution (~30s)
 % map = cr_coherence_vs_psd(val, PeakPos, PowerSpec, CBF);
 
@@ -73,12 +76,12 @@ CBF.f = 5; % subsample factor
 % Bin into 100 bins.  
 CBF.n = 100;  % Number of bins to segment the powerspectrum into
 
-% May not work outside of Kavlifarm (~ 10min in kavlifarm using the parallel pool). Computationally heavy. 
-cr_coherence_all(data, Pxx, CBF)
+% % May not work outside of Kavlifarm (~ 10min in kavlifarm using the parallel pool). Computationally heavy. 
+% cr_coherence_all(data, Pxx, CBF)
+% 
+% save([CBF.targetP, CBF.name, '_CBF_parameters'], 'CBF');
 
-save([CBF.targetP, CBF.name, '_CBF_parameters'], 'CBF');
-
-% %% Step 4.1: Pairwise Coherence versus distance - One patch
+%% Step 4.1: Pairwise Coherence versus distance - One patch
 % 
 % patch_no = 6; 
 % % May not work outside of Kavlifarm. Computationally heavy. 
@@ -95,18 +98,25 @@ CBF.minsize = 400;
 I = freqsBinned.*CBF.mask;
 
 % Run the frequency segmentation (~40s)
-[CBF.lmatrix,complist] = identify_frequency_patchesV2(I, CBF); 
+[CBF.lmatrix,complist] = identify_frequency_patchesV3(I, CBF); 
 
 % Save the results
 save([CBF.targetP, CBF.name, '_CBF_parameters'], 'CBF');
 
 %% Step 6: Calculate wavedirection and wavelength. 
 
+% Decide whether you want to plot the patch check
+check = true; 
+
+% Set the wavelength histogram variables
+CBF.max_wavelength = 200;
+CBF.bin_size = 30;
+
 % Find the phases for all patches (~50s)
 [phase_patch, pos] = calc_phase_patch(data,complist, CBF);
 
 % Calculate the gradient, wave direction and wavelength (~40s)
-[CBF.max_wavelength, CBF.bin_size] = cr_patchwise_analysisV2(phase_patch, pos, PowerSpec, CBF); 
+[CBF] = cr_patchwise_analysisV4(phase_patch, pos, PowerSpec, check, CBF); 
 
 % Save the results
 save([CBF.targetP, CBF.name, '_CBF_parameters'], 'CBF');
